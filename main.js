1ecab6bb1a3c70ad2950a6ec898cadc3be670ef8
Shauryaditya13 <71700044+Shauryaditya13@users.noreply.github.com>
2021-04-11 10:56:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function take_snapshot() {
 }
 
 console.log("ml5 version"+ml5.version);
-classifier=ml5.imageClassifier("",modelloaded);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/s1jbRClwh/model.json",modelloaded);
 
 function modelloaded() {
     console.log("modelloaded");
@@ -28,4 +28,55 @@ function speak() {
     var utterance=new SpeechSynthesisUtterance(speakdata1+speakdata2);
     synth.speak(utterance);
 }
-speak();
+
+function check() {
+    img=document.getElementById("capture_image");
+    classifier.classify(img,getresult);
+}
+
+function getresult(error,results) {
+    if(error) {
+        console.log(error)
+    }
+    else {
+        console.log(results)
+        prediction1=results[0].label;
+        prediction2=results[1].label;
+        document.getElementById("result_emotion_name").innerHTML=prediction1;
+        document.getElementById("result_emotion_name2").innerHTML=prediction2;
+        speak();
+
+        if(prediction1=="Happy") {
+            document.getElementById("update_emoji").innerHTML="&#128522;";
+        }
+
+        if(prediction1=="Sad") {
+            document.getElementById("update_emoji").innerHTML="&#128532;";
+        }
+
+        if(prediction1=="Angry") {
+            document.getElementById("update_emoji").innerHTML="&#128545;";
+        }
+
+        if(prediction1=="Crying") {
+            document.getElementById("update_emoji").innerHTML="&#128546;";
+        }
+
+
+        if(prediction2=="Happy") {
+            document.getElementById("update_emoji2").innerHTML="&#128522;";
+        }
+
+        if(prediction2=="Sad") {
+            document.getElementById("update_emoji2").innerHTML="&#128532;";
+        }
+
+        if(prediction2=="Angry") {
+            document.getElementById("update_emoji2").innerHTML="&#128545;";
+        }
+
+        if(prediction2=="Crying") {
+            document.getElementById("update_emoji2").innerHTML="&#128546;";
+        }
+    }
+}
